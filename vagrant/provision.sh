05cc5a62c2ee9989e48f321c4ce6df37c56a8d9b
Trent <trent.brunson@gmail.com>
2014-10-07 14:26:31 -0400

    Added -y flags to apt-get install commands in provision.sh

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
-sudo apt-get install clang-3.4 lldb-3.4
-sudo apt-get install clang-3.4 clang-3.4-doc libclang-common-3.4-dev libclang-3.4-dev libclang1-3.4 libclang1-3.4-dbg libllvm-3.4-ocaml-dev libllvm3.4 libllvm3.4-dbg lldb-3.4 llvm-3.4 llvm-3.4-dev llvm-3.4-doc llvm-3.4-examples llvm-3.4-runtime clang-modernize-3.4 clang-format-3.4 python-clang-3.4 lldb-3.4-dev
+sudo apt-get install clang-3.4 lldb-3.4 -y
+sudo apt-get install clang-3.4 clang-3.4-doc libclang-common-3.4-dev libclang-3.4-dev libclang1-3.4 libclang1-3.4-dbg libllvm-3.4-ocaml-dev libllvm3.4 libllvm3.4-dbg lldb-3.4 llvm-3.4 llvm-3.4-dev llvm-3.4-doc llvm-3.4-examples llvm-3.4-runtime clang-modernize-3.4 clang-format-3.4 python-clang-3.4 lldb-3.4-dev -y
 
-sudo apt-get install bison flex
-sudo apt-get install llvm-dev
+sudo apt-get install bison flex -y
+sudo apt-get install llvm-dev -y
