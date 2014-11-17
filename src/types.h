32e55b3da22e7fa784fa9ff4d1ac0df7560e8bae
Trent <trent.brunson@gmail.com>
2014-11-17 10:21:35 -0500

    Updating local branch with changes to master

--- src/types.h
+++ src/types.h
@@ -35,26 +35,15 @@ class Type {
 protected:
     void setType(int type);
 public:
-<<<<<<< HEAD
-    bool list; /**< Bool value if the type is a list-type */
+    bool isList; /**< Bool value if the type is a list-type */
     bool structt; /**< Bool value if the type is a struct type */
     TypeCodes typecode; /**< typecode for the Type */
     virtual ~Type() { }
 Type() : typecode(INVALID) { }
-    Type(int type) { setType(type); list = false; structt = false; }
-    Type(int type, bool l) { setType(type); list = l; structt = false; }
-    Type(Type & t, bool l) { typecode = t.typecode; list = l; structt = false; }
-    bool isList() { return list; }
-=======
-    bool isList; /**< Bool value if the type is a list-type */
-    TypeCodes typecode; /**< typecode for the Type */
-    virtual ~Type() { }
-Type() : typecode(INVALID) { }
-    Type(int type) { setType(type); isList = false; }
-    Type(int type, bool l) { setType(type); isList = l; }
-    Type(Type & t, bool l) { typecode = t.typecode; isList = l; }
+    Type(int type) { setType(type); isList = false; structt = false; }
+    Type(int type, bool l) { setType(type); isList = l; structt = false; }
+    Type(Type & t, bool l) { typecode = t.typecode; isList = l; structt = false; }
     bool getIsList() { return isList; }
->>>>>>> Changed 'bool list' var to 'bool isList' and 'bool isList' function to 'bool getIsList'
     llvm::Type * toLlvmType();
     virtual std::ostream & print(std::ostream & os) const;
     friend std::ostream & operator<<(std::ostream & os, Type & type); 
